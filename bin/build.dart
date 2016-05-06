88bbab8ea6a218a17d3c0af380ed6b9f42292ea3
Devon Carew <devoncarew@google.com>
2016-05-06 16:56:03 -0700

    run more iterations of the analyzer benchmarks

--- bin/build.dart
+++ bin/build.dart
@@ -16,7 +16,7 @@ Future<Null> main(List<String> args) async {
   if (args.length != 1)
     fail('Expects a single argument pointing to the root directory of the dashboard but got: ${args}');
 
-  config = new Config(args.single);
+  config = new Config(path.normalize(path.absolute(args.single)));
 
   Chain.capture(() async {
     section('Build started on ${new DateTime.now()}');
@@ -30,6 +30,10 @@ Future<Null> main(List<String> args) async {
     await build();
 
     section('Build finished on ${new DateTime.now()}');
+  }, onError: (error, Chain chain) {
+    print(error);
+    print(chain.terse);
+    exit(1);
   });
 }
 
@@ -63,7 +67,11 @@ Future<Null> getFlutter() async {
 
   await exec('git', ['clone', '--depth', '1', 'https://github.com/flutter/flutter.git']);
   await flutter('config', options: ['--no-analytics']);
+
+  section('flutter doctor');
   await flutter('doctor');
+
+  section('flutter update-packages');
   await flutter('update-packages');
 }
 
@@ -115,32 +123,77 @@ Future<int> runStartupTest(String testDirectory, String testName) {
 Future<Null> runAnalyzerTests() async {
   DateTime now = new DateTime.now();
   String sdk = await getDartVersion();
+  String commit = await getFlutterRepoCommit();
 
-  section('flutter analyze --flutter-repo');
-  File benchmark = file(path.join(config.flutterDirectory.path, 'analysis_benchmark.json'));
-  rm(benchmark);
-  await inDirectory(config.flutterDirectory, () async {
-    await flutter('analyze', options: ['--flutter-repo', '--benchmark']);
-  });
+  Benchmark benchmark = new FlutterAnalyzeBenchmark(now, sdk, commit);
+  section(benchmark.name);
+  await runBenchmark(benchmark, iterations: 3);
 
-  _patchupAnalysisResult(benchmark, now, expected: 25.0, sdk: sdk);
-  copy(benchmark, config.dataDirectory, name: 'analyzer_cli__analysis_time.json');
-
-  section('analysis server mega_gallery');
-  Directory megaDir = dir(path.join(config.flutterDirectory.path, 'dev/benchmarks/mega_gallery'));
-  benchmark = file(path.join(megaDir.path, 'analysis_benchmark.json'));
-  rm(benchmark);
-  await inDirectory(config.flutterDirectory, () async {
-    await dart(['dev/tools/mega_gallery.dart']);
-  });
-  await inDirectory(megaDir, () async {
-    await flutter('analyze', options: ['--watch', '--benchmark']);
-  });
-  _patchupAnalysisResult(benchmark, now, expected: 10.0, sdk: sdk);
-  copy(benchmark, config.dataDirectory, name: 'analyzer_server__analysis_time.json');
+  benchmark = new FlutterAnalyzeAppBenchmark(now, sdk, commit);
+  section(benchmark.name);
+  await runBenchmark(benchmark, iterations: 3);
 }
 
-void _patchupAnalysisResult(File jsonFile, DateTime now, { double expected, String sdk}) {
+class FlutterAnalyzeBenchmark extends Benchmark {
+  FlutterAnalyzeBenchmark(this.time, this.sdk, this.commit) : super('flutter analyze --flutter-repo');
+
+  final DateTime time;
+  final String sdk;
+  final String commit;
+
+  File get benchmarkFile => file(path.join(config.flutterDirectory.path, 'analysis_benchmark.json'));
+
+  @override
+  Future<num> run() async {
+    rm(benchmarkFile);
+    await inDirectory(config.flutterDirectory, () async {
+      await flutter('analyze', options: ['--flutter-repo', '--benchmark']);
+    });
+    return _patchupAnalysisResult(benchmarkFile, time, expected: 25.0, sdk: sdk, commit: commit);
+  }
+
+  @override
+  void markLastRunWasBest(num result) {
+    copy(benchmarkFile, config.dataDirectory, name: 'analyzer_cli__analysis_time.json');
+  }
+}
+
+class FlutterAnalyzeAppBenchmark extends Benchmark {
+  FlutterAnalyzeAppBenchmark(this.time, this.sdk, this.commit) : super('analysis server mega_gallery');
+
+  final DateTime time;
+  final String sdk;
+  final String commit;
+
+  Directory get megaDir => dir(path.join(config.flutterDirectory.path, 'dev/benchmarks/mega_gallery'));
+  File get benchmarkFile => file(path.join(megaDir.path, 'analysis_benchmark.json'));
+
+  Future<Null> init() {
+    return inDirectory(config.flutterDirectory, () async {
+      await dart(['dev/tools/mega_gallery.dart']);
+    });
+  }
+
+  @override
+  Future<num> run() async {
+    rm(benchmarkFile);
+    await inDirectory(megaDir, () async {
+      await flutter('analyze', options: ['--watch', '--benchmark']);
+    });
+    return _patchupAnalysisResult(benchmarkFile, time, expected: 20.0, sdk: sdk, commit: commit);
+  }
+
+  @override
+  void markLastRunWasBest(num result) {
+    copy(benchmarkFile, config.dataDirectory, name: 'analyzer_server__analysis_time.json');
+  }
+}
+
+num _patchupAnalysisResult(File jsonFile, DateTime now, {
+  double expected,
+  String sdk,
+  String commit
+}) {
   Map<String, dynamic> json;
   if (jsonFile.existsSync())
     json = JSON.decode(jsonFile.readAsStringSync());
@@ -148,11 +201,15 @@ void _patchupAnalysisResult(File jsonFile, DateTime now, { double expected, Stri
     json = <String, dynamic>{};
 
   json['timestamp'] = now.millisecondsSinceEpoch;
-  if (sdk != null)
-    json['sdk'] = sdk;
   if (expected != null)
     json['expected'] = expected;
+  if (sdk != null)
+    json['sdk'] = sdk;
+  if (commit != null)
+    json['commit'] = expected;
   jsonFile.writeAsStringSync(jsonEncode(json));
+
+  return json['time'];
 }
 
 Future<Null> generateBuildInfo() async {
@@ -169,4 +226,39 @@ Future<Null> uploadDataToFirebase() async {
   for (File file in ls(config.dataDirectory)) {
     await uploadToFirebase(file);
   }
+}
+
+abstract class Benchmark {
+  Benchmark(this.name);
+
+  final String name;
+
+  Future<Null> init() => new Future.value();
+
+  Future<num> run();
+
+  void markLastRunWasBest(num result);
+
+  String toString() => name;
+}
+
+Future<num> runBenchmark(Benchmark benchmark, { int iterations: 1 }) async {
+  await benchmark.init();
+
+  num minValue;
+
+  while (iterations > 0) {
+    iterations--;
+
+    print('');
+
+    num result = await benchmark.run();
+
+    if (minValue == null || result < minValue) {
+      benchmark.markLastRunWasBest(result);
+      minValue = result;
+    }
+  }
+
+  return minValue;
 }
